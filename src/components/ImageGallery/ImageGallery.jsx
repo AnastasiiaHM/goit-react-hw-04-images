901496f6670ba1e450f6c76e9d6d41f4a2247207
AnastasiiaHM <119488781+AnastasiiaHM@users.noreply.github.com>
2023-03-28 10:24:42 +0200

    fixed load more

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -15,14 +15,16 @@ export function ImageGallery({ inputValue }) {
     if (!inputValue) return;
 
     async function fetchImages() {
-      setStatus('pending');
       try {
+        setStatus('pending');
         const response = await getImages(inputValue, page);
         const data = response.data.hits;
+
         if (page > 1) {
           setHits(prev => [...prev, ...data]);
+        } else {
+          setHits(data);
         }
-        setHits(data);
         setPageQuantity(() => response.data.total / 12);
         setStatus('response');
       } catch {
@@ -40,37 +42,34 @@ export function ImageGallery({ inputValue }) {
   if (status === 'idle') {
     return <Sorry>Hi! Please enter your request!</Sorry>;
   }
-  if (status === 'pending') {
-    return (
-      <Audio
-        height="80"
-        width="80"
-        radius="9"
-        color="green"
-        ariaLabel="loading"
-      />
-    );
-  }
-  if (status === 'response') {
-    return (
-      <>
-        <Gallery>
-          {hits.map(({ id, webformatURL, tags, largeImageURL }) => {
-            return (
-              <ImageGalleryItem
-                key={id}
-                image={webformatURL}
-                alt={tags}
-                largeImage={largeImageURL}
-              />
-            );
-          })}
-        </Gallery>
-        {pagesQuantity > 1 && <BtnLoadMore onClick={handleClickLoadMore} />}
-      </>
-    );
-  }
+
   if (status === 'rejected') {
     return <Sorry>Sorry but we dont find image for you :(</Sorry>;
   }
+  return (
+    <>
+      <Gallery>
+        {hits.map(({ id, webformatURL, tags, largeImageURL }) => {
+          return (
+            <ImageGalleryItem
+              key={id}
+              image={webformatURL}
+              alt={tags}
+              largeImage={largeImageURL}
+            />
+          );
+        })}
+      </Gallery>
+      {status === 'pending' && (
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="green"
+          ariaLabel="loading"
+        />
+      )}
+      {pagesQuantity > 1 && <BtnLoadMore onClick={handleClickLoadMore} />}
+    </>
+  );
 }
